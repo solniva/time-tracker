460fabfad3ef5505af5cac9c0da34cd105ee374d
Sunniva Kinnerud <solniva1@icloud.com>
2026-09-15 13:29:24 +0200

    ...

--- page.py
+++ page.py
@@ -5,10 +5,11 @@ app = Application()
 
 @app.page()
 class TimeTrackerPage(Page):
+    def initial(self):
+        t.h2(f"Omkretsen av en sirkel med radius 2 er {kd.omkrets(2)}")
+        
 
     def populate(self):
         t.h1("Hello, World!")
-        t.h2("hei på deg")
-        #t.h2(f"Omkretsen av en sirkel med radius 2 er {kd.omkrets(2)}")
 
 app.mount("#app")
