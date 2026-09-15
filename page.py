8a468282ac4184bebdcf365833ebbd3d10233f7e
Sunniva Kinnerud <solniva1@icloud.com>
2026-09-15 13:37:29 +0200

    ...

--- page.py
+++ page.py
@@ -1,12 +1,12 @@
 from puepy import Application, Page, t
-import code as kd
+import utils
 
 app = Application()
 
 @app.page()
 class TimeTrackerPage(Page):
     def initial(self):
-        t.h2(f"Omkretsen av en sirkel med radius 2 er {kd.omkrets(2)}")
+        t.h2(f"Omkretsen av en sirkel med radius 2 er {utils.omkrets(2)}")
         
 
     def populate(self):
